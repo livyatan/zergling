f8b74b81f71254a6b62df012315152978af562f2
Kevin Jing Qiu <kevin@idempotent.ca>
2016-06-08 20:32:53 -0400

    Code

--- zergling.py
+++ zergling.py
@@ -1,7 +1,7 @@
 from __future__ import print_function
 
 import os
-# import praw
+import praw
 import click
 import couchdb
 
@@ -33,6 +33,39 @@ def cmd_vote(action, userid, thing):
     print('{} vote {} with user {}'.format(action, thing, userid))
     print('Retrieving zergling')
     zergling = db[userid]
+    reddit = praw.Reddit(user_agent='deadbeefcafebabe')
+    print(zergling['username'])
+    print(zergling['ptpwd'])
+    try:
+        print('Logging in as {}'.format(userid))
+        reddit.login(zergling['username'], zergling['ptpwd'],
+                     disable_warning=True)
+        print('Logging succeeded'.format(userid))
+    except praw.errors.InvalidUserPass:
+        print('User {} not valid'.format(userid))
+        del db[userid]
+        print('User {} deleted'.format(userid))
+    else:
+        submission = reddit.get_submission(thing)
+        redditor = reddit.get_redditor(userid)
+
+        if action == 'up':
+            submission.upvote()
+            print('Upvoted')
+            zergling['upvoted'] = zergling.get('upvoted', [])
+            zergling['upvoted'].append(thing)
+            if thing in set([s.permalink for s in redditor.get_liked()]):
+                print('Upvote recorded')
+        elif action == 'down':
+            submission.downvote()
+            print('Downvoted')
+            zergling['downvoted'] = zergling.get('downvoted', [])
+            zergling['downvoted'].append(thing)
+            if thing in set([s.permalink for s in redditor.get_disliked()]):
+                print('Downvote recorded')
+
+        db.save(zergling)
+        redditor.get_liked
 
 
 if __name__ == '__main__':
